79749e6aa87423329445863fcbb58039483f5f2f
Vu Quang Nguyen <vuquangnguyen2019@yahoo.com>
2022-08-07 21:15:20 +0700

    UPDATE

--- ExtractTimestamp.sql
+++ ExtractTimestamp.sql
@@ -1,1 +1,16 @@
-d
+-- tach NAM
+SELECT EXTRACT(YEAR FROM payment_date)
+FROM payment;
+-- tach Thang
+SELECT EXTRACT(MONTH FROM payment_date)
+FROM payment;
+-- tach Quy
+SELECT EXTRACT(QUARTER FROM payment_date)
+FROM payment;
+-- tach NGAY
+SELECT EXTRACT(DAY FROM payment_date)
+FROM payment;
+
+-- CHUYEN DATE -> STRING
+SELECT TO_CHAR(payment_date,'dd-MM-YYYY') AS payment_date
+FROM payment
